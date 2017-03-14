2eb8382b0a4222dd369f668c35134a064ec78840
Raman But-Husaim <raman_but-husaim@live.com>
2017-03-14 23:50:58 +0300

    Ch02: add hello and afile_server

---
+++ chapter02/src/afile_server.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%%% @author raman
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Mar 2017 11:39 PM
+%%%-------------------------------------------------------------------
+-module(afile_server).
+-author("raman").
+
+%% API
+-export([start/1, loop/1]).
+
+start(Dir) ->
+  spawn(afile_server, loop, [Dir]).
+
+loop(Dir) ->
+  receive
+    {Client, list_dir} ->
+      Client ! {self(), file:list_dir(Dir)};
+    {Client, {get_file, File}} ->
+      Full = filename:join(Dir, File),
+      Client ! {self(), file:read_file(Full)}
+  end,
+  loop(Dir).
---
+++ chapter02/src/hello.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author raman
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Mar 2017 11:27 PM
+%%%-------------------------------------------------------------------
+-module(hello).
+-author("raman").
+
+%% API
+-export([start/0]).
+
+start() ->
+  io:format("Hello world~n").
