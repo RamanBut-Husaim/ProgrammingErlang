37cf9b0d087d399eb8f1c829951262aceca24091
Raman But-Husaim <raman_but-husaim@live.com>
2017-03-14 23:58:49 +0300

    Ch02: add afile_client

---
+++ chapter02/src/afile_client.erl
@@ -0,0 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% @author raman
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Mar 2017 11:52 PM
+%%%-------------------------------------------------------------------
+-module(afile_client).
+-author("raman").
+
+%% API
+-export([ls/1, get_file/2]).
+
+ls(Server) ->
+  Server ! {self(), list_dir},
+  receive
+    {Server, FileList} ->
+      FileList
+  end.
+
+get_file(Server, File) ->
+  Server ! {self(), {get_file, File}},
+  receive
+    {Server, Content} ->
+      Content
+  end.
